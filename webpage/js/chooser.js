9f8228b391637e258b16c8bfc51b4fe463e91a8c
EmaLou <loujia1991@gmail.com>
2014-03-04 17:10:26 +0800

    use bootstrap to implement lunch chooser

--- webpage/js/chooser.js
+++ webpage/js/chooser.js
@@ -2,10 +2,13 @@
  * Created by twer on 2/20/14.
  */
 $(document).ready(function(){
-    $("button").click(function(){
-
+    $(".button").click(function(){
         var lunch_array=["Little Yellow Chicken", "1F Restaurant","Saozi Noodles","FanJia ColdNoodles","Mao Cai","Korea Banfan","Dumplings","GuanZhong Noodles","Xi'an Fanzhuang", "Subway"]
         var num = parseInt(Math.random() * 10-1);
         alert("Congratulation! Your lunch is " + lunch_array[num]);
+        showPicture();
     });
+    function showPicture() {
+        
+    }
 });
